dfb8e10000ab085b98d84dcf6007af284df6a025
baracinha <francisco.baracinha@gmail.com>
2026-04-16 20:45:38 +0100

    ghjfdgdgrtvrt

---
+++ hotel/Controllers/SalesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hotel.Controllers
+{
+    public class SalesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
