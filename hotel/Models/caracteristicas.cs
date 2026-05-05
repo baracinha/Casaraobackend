1f2a1d36fc1bc7310c9dc3ffbb5cf32272231aef
baracinha <francisco.baracinha@gmail.com>
2026-05-05 14:21:20 +0100

    novo css tudo a funcionar

---
+++ hotel/Models/caracteristicas.cs
@@ -0,0 +1,12 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace hotel.Models
+{
+    [Table("caracteristicas")]
+    public class caracteristicas
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+    }
+}
---
+++ hotel/Models/imagens_propriedades.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace hotel.Models
+{
+    [Table("imagens_propriedades")]
+    public class imagens_propriedades
+    {
+        public int id { get; set; }
+        public int id_propriedade { get; set; }
+        public string image_url { get; set; }
+        public propriedades propriedade { get; set; }
+    }
+}
